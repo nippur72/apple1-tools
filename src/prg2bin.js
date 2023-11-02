653a1bc460e67570ef2c23af686a844970024588
nino-porcino <nino.porcino@gmail.com>
2023-11-02 10:49:05 +0100

    Extract tools from apple1-videocard-lib

---
+++ src/prg2bin.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const parseOptions = require("./parseOptions");
+
+const options = parseOptions([
+   { name: 'input', alias: 'i', type: String },
+   { name: 'output', alias: 'o', type: String }
+]);
+
+if(options.input === undefined || options.output === undefined) {
+   console.log("usage: prg2bin -i inputfile.prg -o outputfile.bin");
+   process.exit(-1);
+}
+
+let prg = fs.readFileSync(options.input);
+
+prg = prg.slice(2);
+
+fs.writeFileSync(options.output,prg);
+
+console.log(`${options.output} written`);
+
